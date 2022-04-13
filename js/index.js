9f3e2e435f9f1acdccbedefa39273305db38a359
Giuseppe Cespedes <Giuseppe@Giuseppes-MacBook-Pro.local>
2022-04-13 18:09:52 -0400

    update with task 5 steps

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const newTask = new taskManager();
 
 // form elements
 const taskForm = document.getElementById('taskForm');
+const taskList = document.getElementById('taskList'); // taskList id 
 const title = document.getElementById('task-title');
 const assigned = document.getElementById('assignee');
 const dueDate = document.getElementById('task-date');
@@ -15,42 +16,60 @@ const btn = document.getElementById('submit');
 function checkFields() { // would a switch also work here?
     // would like to show all blank fields, not just one at a time
     if(title.value === '') {
-        alert('TITLE field cannot be left blank.');
+        alert('TASK TITLE field cannot be left blank.');
         return false;
     } 
 
     if(assigned.value === '') {
-        alert('ASSIGNED cannot be left blank.');
+        alert('ASSIGNED TO field cannot be left blank.');
         return false;
     } 
 
     if(dueDate.value === '') {
-        alert('DATE field cannot be left blank.');
+        alert('DUE DATE field cannot be left blank.');
         return false;
     } 
 
     if(description.value === '') {
         alert('DESCRIPTION field cannot be left blank.');
         return false;
-    } 
+    }
+    return true;
 }
 
 // form actions
 taskForm.addEventListener('submit',(event) => {
-    // perform validation
-    checkFields();
+    // perform validation; need to break if condition not met; still creating blank objects for array
+    // checkFields(); 
 
-    // Make sure to prevent the default action of the form!
-    event.preventDefault();
+    // // Make sure to prevent the default action of the form!
+    // event.preventDefault();
     
-    // if validation successful, call taskManager.addTask() method
-    newTask.addTask(title.value,description.value,assigned.value,dueDate.value,taskStatus);
-    console.log(newTask.tasks);
+    // // if validation successful, call taskManager.addTask() method
+    // newTask.addTask(title.value,description.value,assigned.value,dueDate.value,taskStatus);
+    // console.log(newTask.tasks);
     
-    // restore form element's default values
-    taskForm.reset();
+    // // restore form element's default values
+    // taskForm.reset();
 
+    if (checkFields() === true) {
+        event.preventDefault();
+        newTask.addTask(title.value,description.value,assigned.value,dueDate.value,taskStatus);
+        console.log(newTask.tasks);
+        newTask.render();
+        const taskHtml = createTaskHtml(title.value,description.value,assigned.value,dueDate.value,taskStatus);
+        console.log(taskHtml);
+        taskList.innerHTML = taskHtml;
+        taskForm.reset();
+    } else {
+        event.preventDefault();
+    }
 });
 
+// html variable referencing createTaskHtml function in taskManager.js
+// const taskHtml = createTaskHtml(title.value,description.value,assigned.value,dueDate.value,taskStatus);
+// console.log(taskHtml);
 
 
+
+// taskList.innerHTML = taskHtml;
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,22 @@
+// html block with template literals
+const createTaskHtml = (name, description, assignedTo, dueDate, status = 'TODO') =>
+    `
+    <li class="list-group-item">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${description}</p>
+                <p class="card-text">${assignedTo}</p>
+                <p class="card-text">${dueDate}</p>
+                <a href="#" class="btn btn-danger">Delete</a>
+                <span class="badge badge-success">${status}</span>
+                <!-- <a href="#" class="btn btn-success">Update</a> -->
+            </div>
+        </div>
+    </li>
+`;
+
+// create taskManager class
 class taskManager {
     constructor(currentId = 0) {
         this.tasks = []; // task array
@@ -23,4 +42,29 @@ class taskManager {
         // increment the id by 1
         this.currentId ++; 
     }
+
+    render() {
+        let tasksHtmlList = [];
+        // loop through each task in task list and store in a variable
+        for (let i = 0; i < this.tasks.length; i ++) {
+            let task = this.tasks[i];
+            // pass task due date to date constructor
+            let date = new Date(task.dueDate); 
+            // create variable with readable string representing the date
+            let formattedDate = date.toString();
+            // create variable to store the HTML of the current task
+            let taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status);
+            // push taskHtml to tasksHtmlList
+            tasksHtmlList.push(taskHtml);
+        }
+        // after looping through each task, set a variable to string of html for all the tasks in tasksHtmlList
+        let tasksHtml = tasksHtmlList.join("\n"); 
+        console.log(tasksHtml);
+    }
 }
+
+/*
+JavaScript Date objects represent a single moment in time in a platform-independent format. Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
+
+JavaScript Date objects represent a single moment in time in a platform-independent format. Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
+*/
